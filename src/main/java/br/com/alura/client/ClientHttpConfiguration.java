a9f88aeccf42e06280930fbad9bfcf544f5838b0
Flávia Rasquinha <flavia.rask@gmail.com>
2024-01-22 14:39:28 -0300

    Implementação Command

--- src/main/java/br/com/alura/client/ClientHttpConfiguration.java
+++ src/main/java/br/com/alura/client/ClientHttpConfiguration.java
@@ -1,6 +1,5 @@
 package br.com.alura.client;
 
-import br.com.alura.domain.Abrigo;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
@@ -11,8 +10,10 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class ClientHttpConfiguration {
+
     public HttpResponse<String> dispararRequisicaoGet(String uri) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
+
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(uri))
                 .method("GET", HttpRequest.BodyPublishers.noBody())
@@ -20,8 +21,9 @@ public class ClientHttpConfiguration {
         return client.send(request, HttpResponse.BodyHandlers.ofString());
     }
 
-    public HttpResponse<String>  dispararRequisicaoPost(String uri, Object object) throws IOException, InterruptedException {
+    public HttpResponse<String> dispararRequisicaoPost(String uri, Object object) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
+
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(uri))
                 .header("Content-Type", "application/json")
--- src/main/java/br/com/alura/domain/Abrigo.java
+++ src/main/java/br/com/alura/domain/Abrigo.java
@@ -1,14 +1,69 @@
 package br.com.alura.domain;
 
-public class Abrigo {
+import java.util.Arrays;
 
+public class Abrigo {
+    private Long id;
+    private String nome;
+    private String telefone;
+    private String email;
+    private Pet[] pets;
+
+    public Abrigo() {
+
+    }
     public Abrigo(String nome, String telefone, String email) {
         this.nome = nome;
         this.telefone = telefone;
         this.email = email;
     }
-    private String nome;
-    private String telefone;
-    private String email;
+    public Long getId() {
+        return id;
+    }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Pet[] getPets() {
+        return pets;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPets(Pet[] pets) {
+        this.pets = pets;
+    }
+    @Override
+    public String toString() {
+        return "{" +
+                "\"id\":\"" + id + "\"," +
+                "\"nome\":\"" + nome + "\"," +
+                "\"telefone\":\"" + telefone + "\"," +
+                "\"email\":\"" + email + "\"," +
+                "\"pets\":" + Arrays.toString(pets) +
+                "}";
+    }
 }
